042d75e238f303dada1e835b1e00952ca0366ab7
PriyaC <priyacjose@gmail.com>
2019-06-17 18:51:09 -0500

    Committing all the initial changes

---
+++ src/main/java/org/launchcode/cheesemvc/CheeseMvcApplication.java
@@ -0,0 +1,15 @@
+package org.launchcode.cheesemvc;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class CheeseMvcApplication {
+
+	public static void main(String[] args) {
+		run(CheeseMvcApplication.class, args);
+	}
+
+}
